a26c02463709fefd18a124c60f5a5b571a9d1412
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-16 15:23:11 +0300

    One more try

--- src/main/java/managing/tool/authentication/util/JwtUtils.java
+++ src/main/java/managing/tool/authentication/util/JwtUtils.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 @Service
 public class JwtUtils {
 
-    @Value("${jwtSecret}")
+    @Value("${JWT_SECRET}")
     private String SECRET_KEY;
 
     public String extractUsername(String token) {
