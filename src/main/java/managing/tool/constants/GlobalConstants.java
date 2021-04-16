e4153186a5f3862c41c1e1b159dcdfc9e55bf31c
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-16 19:11:57 +0300

    Changing frontend

--- src/main/java/managing/tool/constants/GlobalConstants.java
+++ src/main/java/managing/tool/constants/GlobalConstants.java
@@ -2,7 +2,7 @@ package managing.tool.constants;
 
 public class GlobalConstants {
 
-    public static final String FRONTEND_URL = "https://abmmt-fe.herokuapp.com/";
+    public static final String FRONTEND_URL = "https://abmmt-fe.herokuapp.com";
 //    public static final String FRONTEND_URL = "http://localhost:3200";
     public static final String DUMMY_PASS = "1234";
 
